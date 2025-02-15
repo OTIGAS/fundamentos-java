f4ab599eb842a01b35a97b3af8463c7f35eb16ed
OTIGAS <tfariagouvea@gmail.com>
2025-02-14 22:24:27 -0300

    Palavras Reservadas e Tipos Primitivos

---
+++ src/j05_tipos_primitivos/TiposPrimitivos.java
@@ -0,0 +1,16 @@
+package j05_tipos_primitivos;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        byte b = 0; // b=-128 || 127
+        short s = 0; // s=-32.768 || 32.767
+        int i = 0; // i=-2.147.483.648 || 2.147.483.647
+        long l = 0; // l=-9.223.372.036.854.775.808 || 9.223.372.036.854.775.807
+
+        float f = 0.0F; // f=-3,4028E + 38 || 3,4028E + 38 (6 – 7 dígitos)
+        double d = 0.0; // d=-1,7976E + 308 || 1,7976E + 308 (15 dígitos)
+
+        char    c; // c='\u0000'
+        boolean o; // b=false
+    }
+}
